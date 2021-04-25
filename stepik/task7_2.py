9193f0d6396c99ba7a197a97b0a8b43e80e7a1cb
kivyfreakt <yaloveganikita@outlook.com>
2021-04-25 21:00:02 +0300

    testcommit

--- stepik/task7_2.py
+++ stepik/task7_2.py
@@ -1,14 +1,14 @@
 def prim(n, G):
     visited = [False]*n
     no_edge = 0
-    visited[6] = True
+    visited[0] = True
 
     sum = 0
 
     while (no_edge < n-1):
         min = 101
         y = 0
-
+        x = 0
         for i in range(n):
             if (visited[i]):
                 for j in range(n):
@@ -16,6 +16,8 @@ def prim(n, G):
                         if min > G[i][j]:
                             min = G[i][j]
                             y = j
+                            x = i
+        print(to_str(x), to_str(y))
 
         if min == 101:
             return -1
--- stepik/task7_3.py
+++ stepik/task7_3.py
@@ -31,6 +31,9 @@ class set_union():
         return self.find(v) == self.find(u)
 
 
+def name(s):
+    return chr(s + ord('A'))
+
 def weight_compare(arr):
     return arr[2]
 
@@ -40,16 +43,41 @@ def kruskal(n, m, G):
     G.sort(key = weight_compare)
     for i in range(m):
         if not s.is_same_component(G[i][0], G[i][1]):
-            print(G[i][0], G[i][1])
+            print(name(G[i][0]), name(G[i][1]))
             count += G[i][2]
             s.union(G[i][0], G[i][1])
     return count
 
-n, m = map(int, input().split())
-G = [] # массив ребер
+# n, m = map(int, input().split())
+# G = [] # массив ребер
 
-for i in range(m):
-    v, u, w = map(int, input().split())
-    G.append((v, u, w))
+# for i in range(m):
+#     v, u, w = map(int, input().split())
+#     G.append((v, u, w))
+
+n = 12
+m = 21
+G = [(0, 1, 25),
+(0, 4, 14),
+(1, 2, 16),
+(1, 4, 4),
+(1, 5, 23),
+(2, 5, 8),
+(2, 6, 22),
+(2, 7, 40),
+(2, 3, 36),
+(3, 7, 42),
+(4, 5, 10),
+(4, 8, 28),
+(5, 8, 29),
+(5, 6, 41),
+(5, 9, 35),
+(5, 10, 31),
+(6, 7, 24),
+(7, 10, 5),
+(7, 11, 7),
+(9, 10, 37),
+(10, 11, 6)
+]
 
 print(kruskal(n, m, G))
